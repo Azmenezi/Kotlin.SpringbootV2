92ddf0fd6d6ca264fc8029ef38f639f00e4475dd
azmenezi <azmenezi@gmail.com>
2025-04-13 17:50:05 +0300

    Abdulaziz Alenezi, task 3

--- src/main/kotlin/com/coded/spring/ordering/OrdersController.kt
+++ src/main/kotlin/com/coded/spring/ordering/OrdersController.kt
@@ -1,13 +1,11 @@
 package com.coded.spring.ordering
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class HelloWorldController(
-    val ordersRepository: OrdersRepository
+    val ordersRepository: OrdersRepository,
+    val itemsRepository: ItemsRepository
 ) {
 
     @GetMapping("/hello")
@@ -15,14 +13,30 @@ class HelloWorldController(
 
     @PostMapping("/orders")
     fun submitOrder(@RequestBody request: OrderRequest) {
-        ordersRepository.save(OrderEntity(restaurant = request.restaurant, items = request.items))
+        val items = itemsRepository.findAllByNameIn(request.items)
+
+        val order = OrderEntity(restaurant = request.restaurant, items = items)
+        ordersRepository.save(order)
     }
-    
+
+    @PostMapping("/items")
+    fun createItems(@RequestBody request: CreateItemsRequest): List<ItemsEntity> {
+        val items = request.items.map { ItemsEntity(name = it) }
+        return itemsRepository.saveAll(items)
+    }
+
+
+
+
     @GetMapping("/orders")
     fun getOrders(): MutableList<OrderEntity> = ordersRepository.findAll()
 }
 
 data class OrderRequest(
     val restaurant: String,
+    val items: List<String>
+)
+
+data class CreateItemsRequest(
     val items: List<String>
 )
